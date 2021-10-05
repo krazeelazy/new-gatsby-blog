cd34c523861af5969403656eb13b2533828a1a83
Krazeelazy <mm.mightyme@gmail.com>
2021-10-05 18:43:16 -0400

    adding all our new blog code for gatsby setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions // actions come from Gatsby
+    if (node.internal.type === `MarkdownRemark`) { // if it's a markdown file, make a slug so we can route to it
+        const slug = createFilePath({ node, getNode })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
+    }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(`./src/templates/blog-post.js`), // the template used to make the blog post's page
+                context: {
+                    slug: node.fields.slug,
+                },
+            })
+        })
+    })
+}
---
+++ src/markdown-pages/my-first-post.md
@@ -0,0 +1,9 @@
+---
+title: 'My First Post'
+date: '2021-10-05'
+description: 'My first post :)'
+---
+
+This is my first post. I have embedded a very good video below.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/V-_O7nl0Ii0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
